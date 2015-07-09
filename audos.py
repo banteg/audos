cf7614249b6806fc3a86381416fd5a9297c94df1
banteg <banteeg@gmail.com>
2015-07-09 23:01:11 +0600

    less verbose encoding

--- audos.py
+++ audos.py
@@ -3,6 +3,7 @@ from numpy.fft import rfft, irfft
 from subprocess import call
 import numpy as np
 import sys
+from glob import glob
 import os
 
 '''
@@ -15,6 +16,8 @@ python audos.py [video] [music]
 It is presumed that video has mono sound and audio has stereo sound
 '''
 
+SEARCH_WINDOW = 10  # seconds
+
 
 def silence(samples):
     return np.zeros((samples, 2), dtype=np.int16) # dtype=data.dtype
@@ -22,13 +25,13 @@ def silence(samples):
 
 def extract_wav(in_name, out_name):
     print('extract wav: {}'.format(in_name))
-    cmd = 'ffmpeg -y -i "{}" -ac 1 -ar 44100 "{}"'.format(in_name, out_name)
+    cmd = 'ffmpeg -loglevel panic -y -i "{}" -ac 1 -ar 44100 "{}"'.format(in_name, out_name)
     call(cmd)
 
 
-def mux(video, audio):
+def mux(video, audio, output):
     print('mux to superfile')
-    cmd = 'ffmpeg -y -i "{}" -i "{}" -c copy -map 0:0 -map 1 audos_render.mp4'.format(video, audio)
+    cmd = 'ffmpeg -loglevel panic -y -i "{}" -i "{}" -c copy -map 0:0 -map 1 "{}"'.format(video, audio, output)
     call(cmd)
 
 
@@ -78,17 +81,21 @@ def main():
 
     extract_wav(video, 'tmp_video.wav')
     extract_wav(audio, 'tmp_audio.wav')
-    call('ffmpeg -y -i "{}" -ar 44100 tmp_audio_hq.wav'.format(audio))
+    call('ffmpeg -loglevel panic -y -i "{}" -ar 44100 tmp_audio_hq.wav'.format(audio))
 
     _, vdata = read('tmp_video.wav')
     _, adata = read('tmp_audio.wav')
     _, hqdata = read('tmp_audio_hq.wav')
 
-    adjust = autocorrelation(vdata, adata, 44100 * 10)
+    adjust = autocorrelation(vdata, adata, 44100 * SEARCH_WINDOW)
     sync(hqdata, adjust, len(vdata))
 
-    call('qaac -v256 --no-delay tmp_audio_hq_sync.wav')
-    mux(video, 'tmp_audio_hq_sync.m4a')
+    print('encoding aac')
+    call('qaac -s -v256 --no-delay tmp_audio_hq_sync.wav')
+    mux(video, 'tmp_audio_hq_sync.m4a', '{}_audos.mp4'.format(video))
+
+    for i in glob('tmp_*'):
+        os.unlink(i)
 
 
 if __name__ == '__main__':
